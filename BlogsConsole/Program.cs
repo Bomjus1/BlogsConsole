9ff26491e1a48bdb6a4ee3c4f7008523d1b26642
James Zellmer <jzellmer11@my.wctc.edu>
2019-03-20 14:49:40 -0500

    Added ability to add posts to blogs

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -21,7 +21,7 @@ namespace BlogsConsole
                 var blog = new Blog { Name = name };
 
                 var db = new BloggingContext();
-                db.AddBlog(blog);
+                db.AddBlog(blog);            
                 logger.Info("Blog added - {name}", name);
 
                 // Display all Blogs from the database
@@ -32,6 +32,15 @@ namespace BlogsConsole
                 {
                     Console.WriteLine(item.Name);
                 }
+                Console.Write("Enter a blog you want to post to: ");
+                var blogName = Console.ReadLine();
+                Blog mainBlog = db.Blogs.Where(ID => ID.Name == blogName).FirstOrDefault();
+                Console.Write("Enter title for your post: ");
+                var postTitle = Console.ReadLine();
+                Console.Write("\nEnter content for the post: ");
+                var postEntry/*content*/ = Console.ReadLine();
+                Post mainPost = new Post { Title = postTitle, Content = postEntry, BlogId = mainBlog.BlogId };
+                db.AddPost(mainPost);
             }
             catch (Exception ex)
             {
