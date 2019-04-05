821fb82b11f54be956f532bc5783bc87e7e6f898
James Zellmer <jzellmer11@my.wctc.edu>
2019-04-05 04:58:49 -0500

    display menu added
    added display menu options, and other quality of life changes for blog program

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -15,32 +15,68 @@ namespace BlogsConsole
             {
 
                 // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
+                displayMenu();
+                string input = Console.ReadLine();
                 var db = new BloggingContext();
-                db.AddBlog(blog);            
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                if (input == "1")
                 {
-                    Console.WriteLine(item.Name);
+                    Console.Write("Enter a new Blog: ");
+                    var name = Console.ReadLine();
+
+                    var blog = new Blog { Name = name };
+                    
+                    db.AddBlog(blog);
+                    logger.Info("Blog added - {name}", name);
                 }
-                Console.Write("Enter a blog you want to post to: ");
-                var blogName = Console.ReadLine();
-                Blog mainBlog = db.Blogs.Where(ID => ID.Name == blogName).FirstOrDefault();
-                Console.Write("Enter title for your post: ");
-                var postTitle = Console.ReadLine();
-                Console.Write("\nEnter content for the post: ");
-                var postEntry/*content*/ = Console.ReadLine();
-                Post mainPost = new Post { Title = postTitle, Content = postEntry, BlogId = mainBlog.BlogId };
-                db.AddPost(mainPost);
+                else if (input == "2")
+                {
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+                }
+                else if (input == "3")
+                {
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+
+                    Console.Write("Enter a blog you want to post to: ");
+                    var blogName = Console.ReadLine();
+                    Blog mainBlog = db.Blogs.Where(ID => ID.Name == blogName).FirstOrDefault();
+                    Console.Write("Enter title for your post: ");
+                    var postTitle = Console.ReadLine();
+                    Console.Write("\nEnter content for the post: ");
+                    var postEntry/*content*/ = Console.ReadLine();
+                    Post mainPost = new Post { Title = postTitle, Content = postEntry, BlogId = mainBlog.BlogId };
+                    db.AddPost(mainPost);
+                }
+                else if (input == "4")
+                {
+                    var query2 = db.Posts.OrderBy(p => p.Title);
+
+                    Console.WriteLine("All post titles in the database:");
+                    foreach (var item2 in query2)
+                    {
+                        Console.WriteLine(item2.Title);
+                    }
+                }
+               
+
+                void displayMenu()
+                {
+                    Console.WriteLine("Enter 1 to add a blog: ");
+                    Console.WriteLine("Enter 2 to display all blogs: ");
+                    Console.WriteLine("Enter 3 to create a post: ");
+                    Console.WriteLine("Enter 4 to display all posts: ");                    
+                }
             }
             catch (Exception ex)
             {
